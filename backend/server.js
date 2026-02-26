dfab38d01c2f641743e2b3814b353cddf1f3591a
Oscar Duran <l.dulo050219@itses.edu.mx>
2026-02-26 13:54:41 -0600

    Primer Commit: Backend + PG

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
