cb38f92b797e7e0b54a854a834f500b288673285
abrahamrg27 <l.ruge000627@itses.edu.mx>
2026-03-01 00:53:56 -0600

    Agregado home, login, registor y diseño Chambee

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  currentSlide = 0;
+
+  slides = [
+    {
+      title: 'Desarrollador Frontend',
+      salary: '$20,000 MXN',
+      location: 'Ciudad de México',
+      mode: 'Remoto',
+      description: 'Trabaja con Angular en proyectos modernos.'
+    },
+    {
+      title: 'Backend Developer',
+      salary: '$25,000 MXN',
+      location: 'Guadalajara',
+      mode: 'Híbrido',
+      description: 'Desarrollo con Node.js y PostgreSQL.'
+    },
+    {
+      title: 'UI/UX Designer',
+      salary: '$18,000 MXN',
+      location: 'Monterrey',
+      mode: 'Presencial',
+      description: 'Diseña experiencias modernas.'
+    }
+  ];
+
+  ngOnInit() {
+
+    setInterval(() => {
+
+      this.nextSlide();
+
+    }, 4000);
+
+  }
+
+  nextSlide() {
+
+    this.currentSlide =
+      (this.currentSlide + 1) % this.slides.length;
+
+  }
+
+  prevSlide() {
+
+    this.currentSlide =
+      (this.currentSlide - 1 + this.slides.length)
+      % this.slides.length;
+
+  }
+
+  goToSlide(index:number) {
+
+    this.currentSlide = index;
+
+  }
+
+}
